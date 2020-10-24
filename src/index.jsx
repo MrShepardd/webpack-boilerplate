369cae11b76c65e034d64438a9d16de9be7698b4
Konstantin Travin <49562463+MrShepardd@users.noreply.github.com>
2020-10-24 18:22:57 +1000

    init commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+
+const App = () => (
+  <div className="container">
+    <h1>Webpack</h1>
+  </div>
+)
+
+render(<App/>, document.getElementById('app'))
